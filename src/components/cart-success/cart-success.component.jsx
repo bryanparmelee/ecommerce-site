e149414d835aade07f9d0aa0aa5776ace1c2a256
bryanparmelee <bryan.parmelee@gmail.com>
2022-10-31 23:14:44 -0400

    Create successfully added to cart modal

---
+++ src/components/cart-success/cart-success.component.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+import './cart-success.styles.scss';
+
+const CartSuccess = ({ product, isAddedToCart, setIsAddedToCart }) => {
+    const { title, price, image } = product;
+
+    const { cartTotal, cartCount } = useContext(CartContext);
+
+    const closeCartSuccess = () => setIsAddedToCart(!isAddedToCart);
+
+    return (
+        <div className='cart-success-container'>
+            <div className='success-card'>
+                <div className='success-item'>
+                    <h2>Successfully added to cart!</h2>
+                    <h3>{title}</h3>
+                    <span>{`$${price}`}</span>
+                    <img src={image} alt={`${title}`} />
+                </div>
+                <div className='success-cart'>
+                    <h3>Your cart</h3>
+                    <span>{`${cartCount} items`}</span>
+                    <span>{`Total: $${cartTotal}`}</span>
+                </div>
+                <span className='close-symbol' onClick={closeCartSuccess}>&#10005;</span>
+            </div>
+         
+        </div>
+    )
+}
+
+export default CartSuccess;
--- src/routes/product-page/product-page.component.jsx
+++ src/routes/product-page/product-page.component.jsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ProductsContext } from "../../contexts/products.context";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../../components/button/button.component";
+import CartSuccess from "../../components/cart-success/cart-success.component";
 
 
 
@@ -16,19 +17,30 @@ const ProductPage = () => {
 
     const { title, description, image, price } = product;
 
+    const [isAddedToCart, setIsAddedToCart] = useState(false);
 
-    const addProductToCart = () => addItemToCart(product);
+    const addProductToCart = () => {
+        addItemToCart(product);
+        setIsAddedToCart(!isAddedToCart);
+    }
 
     return (
         <>
-            <div>
+            <div className="product-page-container">
                 <h2>{productId}</h2>
                 <h2>{title}</h2>
                 <img src={image} alt={`${title}`} />
-                <h3>{price}</h3>
+                <h3>{'$' + price}</h3>
                 <span>{description}</span>
                 <Button onClick={addProductToCart}>Add to cart</Button>
+                {isAddedToCart && 
+                    <CartSuccess 
+                        product={product} 
+                        isAddedToCart={isAddedToCart} 
+                        setIsAddedToCart={setIsAddedToCart}                                         
+                        />}
             </div>
+            
         </>
     )
 }
