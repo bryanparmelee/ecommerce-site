82bdbf227da47907bdde278b33f50fbb26cabb4a
bryanparmelee <bryan.parmelee@gmail.com>
2023-02-16 15:50:19 -0500

    Update cart, cart item styling

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,51 +1,47 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
 
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CartContext } from '../../contexts/cart.context';
+const CartItem = ({ cartItem }) => {
+  const { title, image, price, quantity, id } = cartItem;
 
-const CartItem = ({cartItem}) => {
-    const { title, image, price, quantity, id} = cartItem;
+  const { clearItemFromCart, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
 
-    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
+  const clearItemHandler = () => clearItemFromCart(cartItem);
+  const addItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () => removeItemFromCart(cartItem);
 
-    const clearItemHandler = () => clearItemFromCart(cartItem); 
-    const addItemHandler = () => addItemToCart(cartItem);
-    const removeItemHandler = () => removeItemFromCart(cartItem);
+  return (
+    <div className="relative flex min-h-[120px] w-full items-center gap-2 border-b border-gray-600/50 px-2">
+      <img
+        className="w-20 object-contain p-2 sm:w-32"
+        src={image}
+        alt={`${title}`}
+      />
 
-    return (
-        <div className='w-full h-32 px-2 flex items-center border-b border-gray-600/50 relative'>
-            <div className='w-1/3'>
-                <img 
-                    className='w-full max-h-32 object-contain sm:p-2'
-                    src={image} 
-                    alt={`${title}`} 
-                        
-                    />
-            </div>
-          
-            <span className='w-1/3 p-1 ml-4 text-xs font-light'>{title}</span>
-        
-            <div className='w-1/3 p-1 flex flex-col items-end font-bold text-sm sm:text-lg'>
-                <span className='text-red-500'>{`$${price.toFixed(2)}`}</span>
-                <span className='p-0.5 flex'>
-                    <div className='' onClick={removeItemHandler}>
-                        &#10094;
-                    </div>
-                    <div className='mx-3'>
-                        {quantity}
-                    </div>
-                    <div className='' onClick={addItemHandler}>
-                        &#10095;
-                    </div>
-                </span>
-               
-            </div>    
-            <span 
-                className='text-xl text-center font-bold absolute top-0 right-1 cursor-pointer'
-                onClick={clearItemHandler}
-            >&#10005;</span>                
-        </div>       
-    )
-}
+      <span className="ml-4 w-1/3 p-1 text-xs font-light">{title}</span>
+
+      <div className="flex w-1/3 flex-col items-end p-1 text-sm font-bold sm:text-lg">
+        <span className="text-red-500">{`$${price.toFixed(2)}`}</span>
+        <span className="flex p-0.5">
+          <div className="" onClick={removeItemHandler}>
+            &#10094;
+          </div>
+          <div className="mx-3">{quantity}</div>
+          <div className="" onClick={addItemHandler}>
+            &#10095;
+          </div>
+        </span>
+      </div>
+      <span
+        className="absolute top-0 right-1 cursor-pointer text-center text-xl font-bold"
+        onClick={clearItemHandler}
+      >
+        &#10005;
+      </span>
+    </div>
+  );
+};
 
 export default CartItem;
--- src/routes/cart/cart.component.jsx
+++ src/routes/cart/cart.component.jsx
@@ -1,48 +1,46 @@
-import { CartContext } from '../../contexts/cart.context';
-import { useContext } from 'react';
+import { CartContext } from "../../contexts/cart.context";
+import { useContext } from "react";
 
-import Button from '../../components/button/button.component';
-import { BUTTON_TYPE_CLASSES } from '../../components/button/button.component';
+import Button from "../../components/button/button.component";
+import { BUTTON_TYPE_CLASSES } from "../../components/button/button.component";
 
-import CartItem from '../../components/cart-item/cart-item.component';
+import CartItem from "../../components/cart-item/cart-item.component";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-    const { cartItems, cartTotal } = useContext(CartContext);
-    const navigate = useNavigate();
-    const goToShop = () => navigate('../shop');
-    const goToCheckout = () => navigate('/checkout')
+  const { cartItems, cartTotal } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToShop = () => navigate("../shop");
+  const goToCheckout = () => navigate("/checkout");
 
-    return (
-        <div className='w-full p-4 sm:px-64 h-full flex flex-col items-center justify-center'>
-            <h1 className='w-full text-3xl p-3 mb-4 font-bold text-center'>YOUR CART</h1>            
-            <div className='w-full p-2'>
-                {cartItems.length ?
-                    cartItems.map((cartItem) => (                                   
-                            <CartItem key={cartItem.id} cartItem={cartItem} />                               
-                    )                 
-                    ) : (
-                    <div className='w-full h-24 flex flex-col justify-between'>
-                        <span className='text-md'>Your cart is empty</span>
-                        <Button onClick={goToShop}>Start shopping</Button>
-                    </div>
-                )}
-                {cartItems.length && ( 
-                    <>
-                        <div className='w-full text-xl sm:text-3xl font-bold text-right pb-8'>
-                            {cartTotal > 0 && `Total: $${cartTotal.toFixed(2)}`}            
-                        </div>      
-                        <Button
-                            onClick={goToCheckout}
-                        > Go to Checkout
-                        </Button>  
-                    </> 
-                )}
-            </div>
-           
-        </div>
-    )
-}
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 p-4">
+      <h1 className="p-3 text-center text-3xl font-bold sm:text-5xl">
+        Your Cart
+      </h1>
+      <div className="max-w-[600px] p-2">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <div className="flex w-full flex-col items-center gap-4">
+            <span className="text-md">Your cart is currently empty</span>
+            <Button onClick={goToShop}>Start shopping</Button>
+          </div>
+        )}
+      </div>
+      {cartItems.length ? (
+        <>
+          <div className="py-8 text-right text-xl font-bold sm:text-3xl">
+            {cartTotal > 0 && `Total: $${cartTotal.toFixed(2)}`}
+          </div>
+          <Button onClick={goToCheckout}> Go to Checkout</Button>
+        </>
+      ) : null}
+    </div>
+  );
+};
 
 export default Cart;
