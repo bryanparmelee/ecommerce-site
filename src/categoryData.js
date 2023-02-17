c3a4f8553c92ec9626b78f90a838b2a70e285c0a
bryanparmelee <bryan.parmelee@gmail.com>
2023-02-17 15:20:05 -0500

    Fix typo

--- src/categoryData.js
+++ src/categoryData.js
@@ -1,28 +1,28 @@
 const CATEGORY_DATA = [
-    {
-        "id": 20,
-        "category": "womens",
-        "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
-        "route": 'shop/womens'
-    },
-    {
-        "id": 14,
-        "category": "electronics",
-        "image": "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
-        "route": 'shop/electronics',
-    },
-    {
-        "id": 7,        
-        "category": "jewelery",
-        "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
-        "route": 'shop/jewelery',
-    },
-    {
-        "id": 4,      
-        "category": "mens",
-        "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-        "route": 'shop/mens',
-    }
+  {
+    id: 20,
+    category: "womens",
+    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    route: "shop/womens",
+  },
+  {
+    id: 14,
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    route: "shop/electronics",
+  },
+  {
+    id: 7,
+    category: "jewelry",
+    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    route: "shop/jewelry",
+  },
+  {
+    id: 4,
+    category: "mens",
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    route: "shop/mens",
+  },
 ];
 
 export default CATEGORY_DATA;
